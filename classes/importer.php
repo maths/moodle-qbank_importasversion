56a8bd2eea908ed84340241b7e3491ca07167004
Andreas Steiger <andreas.steiger@math.ethz.ch>
2023-06-14 14:12:38 +0200

    Made importer run without errors (but html isn't pretty yet)

---
+++ classes/importer.php
@@ -0,0 +1,209 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace qbank_importasversion;
+
+use context;
+use context_course;
+use core_question\local\bank\plugin_features_base;
+use core_tag_tag;
+use qformat_xml;
+use question_bank;
+
+require_once($CFG->dirroot . '/question/format/xml/format.php');
+
+/**
+ * Information about which features are provided by this plugin.
+ *
+ * @package   qbank_importasversion
+ * @copyright 2023 MootDACH DevCamp
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class importer extends qformat_xml {
+
+    public static function import_file($qformat, $question, $importedquestionfile)  {
+        global $USER, $DB, $OUTPUT;
+
+        $context = context::instance_by_id($question->contextid);
+
+        // Raise time and memory, as importing can be quite intensive.
+        //\core_php_time_limit::raise();
+        //raise_memory_limit(MEMORY_EXTRA);
+
+        // STAGE 1: Parse the file
+        if ($qformat->displayprogress) {
+            echo $OUTPUT->notification(get_string('parsingquestions', 'question'), 'notifysuccess');
+        }
+
+        if (! $importedlines = $qformat->readdata($importedquestionfile)) {
+            echo $OUTPUT->notification(get_string('cannotread', 'question'));
+            return false;
+        }
+
+        //print_object($importedlines);
+
+        if (!$importedquestions = $qformat->readquestions($importedlines)) {   // Extract all the questions
+            echo $OUTPUT->notification(get_string('noquestionsinfile', 'question'));
+            return false;
+        }
+
+        if (count($importedquestions) != 1) { // Check if there's only one question in the file -- remove this once we do batch processing!
+            echo $OUTPUT->notification(get_string('toomanyquestionsinfile', 'qbank_importasversion'));
+            return false;   
+        }
+
+        //print_object($importedquestions);
+
+        // STAGE 2: Write data to database
+        if ($qformat->displayprogress) {
+            echo $OUTPUT->notification(get_string('importingquestions', 'question',
+                $qformat->count_questions($question->id)), 'notifysuccess');
+        }
+
+        // check for errors before we continue
+        if ($qformat->stoponerror and ($qformat->importerrors>0)) {
+            echo $OUTPUT->notification(get_string('importparseerror', 'question'));
+            return true;
+        }
+
+        // check for errors before we continue
+        /*
+        if ($qformat->stoponerror) {
+            return false;
+        }
+        */
+
+        // count number of questions processed
+        $count = 0;
+
+        // for now, single question
+        $importedquestion = $importedquestions[0];
+        //$existingquestion = $DB->get_record('question', ['id' => $existingquestionid]);
+        //$existingquestionversion = $DB->get_record('question_versions', ['questionid' => $question->id]);
+
+        //print_object($question);
+
+        //foreach ($existingquestions as $existingquestion) {   // Process and store each question
+            $transaction = $DB->start_delegated_transaction();
+
+            // reset the php timeout
+            //\core_php_time_limit::raise();
+
+            $count++;
+
+            if ($qformat->displayprogress) {
+                echo "<hr /><p><b>{$count}</b>. " . $qformat->format_question_text($question) . "</p>";
+            }
+
+            $fileoptions = array(
+                    'subdirs' => true,
+                    'maxfiles' => -1,
+                    'maxbytes' => 0,
+                );
+            
+            //print_object($existingquestion);
+
+            $newquestion = $importedquestion;
+            $newquestion->context = $context;
+            $newquestion->id = $DB->insert_record('question', $importedquestion);
+            // Create a version for each question imported.
+            $questionversion = new \stdClass();
+            $questionversion->questionbankentryid = $question->questionbankentryid;
+            $questionversion->questionid = $newquestion->id;
+            $questionversion->version = get_next_version($question->questionbankentryid);
+            $questionversion->status = \core_question\local\bank\question_version_status::QUESTION_STATUS_READY; // TODO: Give an option on the form
+            $questionversion->id = $DB->insert_record('question_versions', $questionversion);
+
+            //$event = \core\event\question_created::create_from_question_instance($newquestion, $qformat->importcontext);
+            //$event->trigger();
+
+            if (isset($newquestion->questiontextitemid)) {
+                $newquestion->questiontext = file_save_draft_area_files($newquestion->questiontextitemid,
+                        $context->id, 'question', 'questiontext', $newquestion->id,
+                        $fileoptions, $newquestion->questiontext);
+            } else if (isset($newquestion->questiontextfiles)) {
+                foreach ($newquestion->questiontextfiles as $file) {
+                    question_bank::get_qtype($newquestion->qtype)->import_file(
+                            $context, 'question', 'questiontext', $newquestion->id, $file);
+                }
+            }
+            if (isset($newquestion->generalfeedbackitemid)) {
+                $newquestion->generalfeedback = file_save_draft_area_files($newquestion->generalfeedbackitemid,
+                        $context->id, 'question', 'generalfeedback', $newquestion->id,
+                        $fileoptions, $newquestion->generalfeedback);
+            } else if (isset($newquestion->generalfeedbackfiles)) {
+                foreach ($newquestion->generalfeedbackfiles as $file) {
+                    question_bank::get_qtype($newquestion->qtype)->import_file(
+                            $context, 'question', 'generalfeedback', $newquestion->id, $file);
+                }
+            }
+            $DB->update_record('question', $newquestion);
+
+            $qformat->questionids[] = $newquestion->id;
+
+            // Now to save all the answers and type-specific options
+
+            $result = question_bank::get_qtype($newquestion->qtype)->save_question_options($newquestion);
+
+            if (core_tag_tag::is_enabled('core_question', 'question')) {
+                // Is the current context we're importing in a course context?
+                $importingcontext = $context;
+                //print_object($question);
+                //echo "hello";
+                $importingcoursecontext = $importingcontext->get_course_context(false);
+                $isimportingcontextcourseoractivity = !empty($importingcoursecontext);
+
+                //print_object($newquestion);
+                if (!empty($newquestion->coursetags)) {
+                    if ($isimportingcontextcourseoractivity) {
+                        $mergedtags = array_merge($newquestion->coursetags, $newquestion->tags);
+
+                        core_tag_tag::set_item_tags('core_question', 'question', $newquestion->id,
+                            $newquestion->context, $mergedtags);
+                    } else {
+                        core_tag_tag::set_item_tags('core_question', 'question', $newquestion->id,
+                            context_course::instance($qformat->course->id), $newquestion->coursetags);
+
+                        if (!empty($newquestion->tags)) {
+                            core_tag_tag::set_item_tags('core_question', 'question', $newquestion->id,
+                                $importingcontext, $newquestion->tags);
+                        }
+                    }
+                } else if (!empty($newquestion->tags)) {
+                    core_tag_tag::set_item_tags('core_question', 'question', $newquestion->id,
+                        $newquestion->context, $newquestion->tags);
+                }
+            }
+
+            if (!empty($result->error)) {
+                echo $OUTPUT->notification($result->error);
+                // Can't use $transaction->rollback(); since it requires an exception,
+                // and I don't want to rewrite this code to change the error handling now.
+                $DB->force_transaction_rollback();
+                return false;
+            }
+
+            $transaction->allow_commit();
+
+            if (!empty($result->notice)) {
+                echo $OUTPUT->notification($result->notice);
+                return true;
+            }
+
+        //}
+        return true;
+    }
+}
--- import.php
+++ import.php
@@ -27,6 +27,7 @@ use qbank_importasversion\form\import_form;
 require_once(__DIR__ . '/../../../config.php');
 require_once($CFG->dirroot . '/question/editlib.php');
 require_once($CFG->dirroot . '/question/format.php');
+require_once(__DIR__ . '/classes/importer.php');
 
 core_question\local\bank\helper::require_plugin_enabled('qbank_importasversion');
 
@@ -78,9 +79,70 @@ if ($importform->is_cancelled()) {
 
 // Handle to form being submitted.
 if ($fromform = $importform->get_data()) {
-    // TODO.
-    throw new coding_exception('We need to implement this!');
-    redirect($returnurl);
+
+    $fromform->format = 'xml';
+
+    //print_object($fromform->contextfromfile);
+
+    // File checks out ok.
+    $fileisgood = false;
+
+    // Work out if this is an uploaded file.
+    // Or one from the filesarea.
+    $realfilename = $importform->get_new_filename('newfile');
+    $importfile = make_request_directory() . "/{$realfilename}";
+    if (!$result = $importform->save_file('newfile', $importfile, true)) {
+        throw new moodle_exception('uploadproblem');
+    }
+    
+    $formatfile = $CFG->dirroot . '/question/format/' . $fromform->format . '/format.php';
+    if (!is_readable($formatfile)) {
+        throw new moodle_exception('formatnotfound', 'question', '', $fromform->format);
+    }
+    
+    require_once($formatfile);
+    
+    $classname = 'qformat_' . $fromform->format;
+    $qformat = new $classname();
+    
+    // Load data into class.
+    /*$qformat->setCategory($category);
+    $qformat->setContexts($contexts->having_one_edit_tab_cap('import'));
+    $qformat->setCourse($COURSE);
+    $qformat->setFilename($importfile);
+    $qformat->setRealfilename($realfilename);
+    $qformat->setMatchgrades($form->matchgrades);
+    $qformat->setCatfromfile(!empty($form->catfromfile));
+    qformat->setContextfromfile(!empty($fromform->contextfromfile));
+    $qformat->setStoponerror($form->stoponerror);*/
+    
+    // Do anything before that we need to.
+    if (!$qformat->importpreprocess()) {
+        throw new moodle_exception('cannotimport', '', $thispageurl->out());
+    }
+
+    qbank_importasversion\importer::import_file($qformat, $question, $importfile);
+
+    // In case anything needs to be done after.
+    if (!$qformat->importpostprocess()) {
+        throw new moodle_exception('cannotimport', '', $thispageurl->out());
+    }
+    
+    // Log the import into this category.
+    /*$eventparams = [
+            'contextid' => $qformat->category->contextid,
+            'other' => ['format' => $form->format, 'categoryid' => $qformat->category->id],
+    ];
+    $event = \core\event\questions_imported::create($eventparams);
+    $event->trigger();
+    */
+    //$params = $urlparams + ['category' => $qformat->category->id . ',' . $qformat->category->contextid];
+    echo $OUTPUT->continue_button($returnurl);
+    //echo $OUTPUT->footer();
+    exit;
+
+
+
 }
 
 // Display the form.
